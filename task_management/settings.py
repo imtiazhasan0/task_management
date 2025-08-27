13d2551f0d13da9424e540ae4571b3187a8bc0ca
imtiazhasan0 <imhasan1062@gamil.com>
2025-08-27 22:37:46 +0600

    initial database setup

--- task_management/settings.py
+++ task_management/settings.py
@@ -74,13 +74,25 @@ WSGI_APPLICATION = 'task_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+#For sqlite
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+#For postgresql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'task_management',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class employee(models.Model):
+    name= models.CharField(max_length=100)
+    email= models.EmailField(unique=True)
+
+
+
+class task(models.Model):
+    project= models.ForeignKey('project', on_delete=models.CASCADE, default=1)
+    assigned_to= models.ManyToManyField(employee)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    is_completed = models.BooleanField(default=False)
+    due_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class taskdetails(models.Model):
+    HIGH, MEDIUM, LOW = 'H', 'M', 'L'
+    PRIORITY=(
+        (HIGH, 'High'),
+        (MEDIUM, 'Medium'),
+        (LOW, 'Low')
+    )
+    task = models.OneToOneField(task, on_delete=models.CASCADE,related_name='details')
+    assigned_to = models.CharField(max_length=100)
+    priority = models.CharField(max_length=1, choices=PRIORITY, default= 'L')
+
+class project(models.Model):
+    name = models.CharField(max_length=200)
+    start_date = models.DateField()
